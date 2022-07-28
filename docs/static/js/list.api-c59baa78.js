8704f9a9f67fd1f3ebf14a131b0b2172e48c4043
wangbin <bin_wang@jsxzkingdee.com>
2022-07-28 17:33:02 +0800

    发布预览

--- docs/static/js/list.api-c59baa78.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-80424e34.js";function r(){return t({url:"/list/base",method:"get"})}function s(){return t({url:"/list/articles",method:"get"})}function i(){return t({url:"/list/projects",method:"get"})}export{i as a,s as b,r as g};
--- docs/static/js/user.api-48ce0fc9.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-80424e34.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
