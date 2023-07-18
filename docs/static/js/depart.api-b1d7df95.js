3e689d75f97283f510c87362d63f5ab1bd88ade4
bin <316281400@qq.com>
2023-07-18 10:45:43 +0800

    提交发布

---
+++ docs/static/js/depart.api-b1d7df95.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-c2794864.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
---
+++ docs/static/js/user.api-680e2e8e.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-c2794864.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
