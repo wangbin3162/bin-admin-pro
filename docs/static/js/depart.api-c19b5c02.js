0cf8b5fed2ff92959c467feb8af5500ebf20706c
bin <316281400@qq.com>
2023-07-31 14:00:56 +0800

    提交发布

--- docs/static/js/depart.api-c19b5c02.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-8ce36798.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
--- docs/static/js/user.api-46b9abcf.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-8ce36798.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
