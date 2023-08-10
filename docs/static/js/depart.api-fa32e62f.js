ab94e6c8d05712445620c7940510e597f40ce897
bin <316281400@qq.com>
2023-08-10 15:56:50 +0800

    提交保存

---
+++ docs/static/js/depart.api-fa32e62f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-05f985aa.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
---
+++ docs/static/js/user.api-13e4a6bf.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-05f985aa.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
