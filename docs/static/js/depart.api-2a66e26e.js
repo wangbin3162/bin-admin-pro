84659e09c6cdc990bd14f8717dc9f79e4b2a5c07
bin <316281400@qq.com>
2023-09-26 17:10:09 +0800

    提交发布

---
+++ docs/static/js/depart.api-2a66e26e.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-fda68aa4.js";function r(){return e({url:"/depart/tree",method:"get"})}export{r as g};
---
+++ docs/static/js/user.api-05439340.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index-fda68aa4.js";function r(){return t({url:"/user/list",method:"get"})}export{r as g};
